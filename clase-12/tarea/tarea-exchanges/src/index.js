4dbd9bb7e250798b2da3da4eecec933e3f1b1935
black753 <mauroo_00@hotmail.com>
2020-02-11 14:31:44 -0300

    Instalacion ESlint

--- clase-12/tarea/tarea-exchanges/src/index.js
+++ clase-12/tarea/tarea-exchanges/src/index.js
@@ -1,48 +1,41 @@
 /// <reference types="jquery"/>
 
-
-
 $.ajax({
-    method: "GET",
-    url: "https://api.exchangeratesapi.io/latest",
-    success: respuesta => {
-        añadirBases(respuesta)
-    },
-    error: error=>{
-        const alerta=$(`<div class="alert alert-danger" role="alert" id="datos-incorrectos">
+  method: 'GET',
+  url: 'https://api.exchangeratesapi.io/latest',
+  success: respuesta => {
+    añadirBases(respuesta)
+  },
+  error: () => {
+    const alerta = $(`<div class="alert alert-danger" role="alert">
         La base de datos se escuentra caida, intentalo mas tarde. Lo sentimos </div>`)
 
-        $("form").append(alerta)
-    }
+    $('form').append(alerta)
+  }
 
 })
 
+$('#convertir').click(() => {
+  $('#devolucion-monedas').empty()
 
+  const fecha = $('#fecha').val()
+  const monedaBase = $('#listado-bases').val()
 
-$("#convertir").click(()=>{
-    $("#devolucion-monedas").empty()
+  const URL = `https://api.exchangeratesapi.io/${fecha}?base=${monedaBase}`
 
-    const fecha=$("#fecha").val()
-    const monedaBase=$("#listado-bases").val()
+  if (monedaBase.length === 3) {
+    if ($('#datos-incorrectos')) {
+      $('#datos-incorrectos').detach()
+    }
 
-    const URL=`https://api.exchangeratesapi.io/${fecha}?base=${monedaBase}`
-
-    if(validarInputs(fecha, monedaBase)){
-
-        if($("#datos-incorrectos")){
-            $("#datos-incorrectos").detach()
-        }
-
-        $.ajax({
-            method: "GET",
-            url: URL,
-            success: respuesta => {
-                
-                Object.keys(respuesta.rates).forEach(moneda => {
-                    
-                    if(moneda!==respuesta.base){
-                        $("#devolucion-monedas").append(
-                            $(`<div class="col">
+    $.ajax({
+      method: 'GET',
+      url: URL,
+      success: respuesta => {
+        Object.keys(respuesta.rates).forEach(moneda => {
+          if (moneda !== respuesta.base) {
+            $('#devolucion-monedas').append(
+              $(`<div class="col">
                                 <div class="card text-center border-dark">
                                     <p style="margin: 12px;">
                                         <strong>${moneda} :</strong>
@@ -50,53 +43,40 @@ $("#convertir").click(()=>{
                                     </p>
                                 </div>
                         </div>`))
-                    }
-                })
-            },
-            error: error => {
-                const alerta=$(`<div class="alert alert-danger" role="alert" id="datos-incorrectos">
-                No pudimos obtener los datos de esa fecha y con esa base. Lo sentimos </div>`)
-        
-                $("form").append(alerta)
-            }
+          }
         })
+      },
+      error: () => {
+        const alerta = $(`<div class="alert alert-danger" role="alert" id="datos-incorrectos">
+                No pudimos obtener los datos de esa fecha y con esa base. Lo sentimos </div>`)
 
-    }else{
+        $('form').append(alerta)
+      }
+    })
+  } else {
+    $('#datos-incorrectos').detach()
 
-        $("#datos-incorrectos").detach()
-
-        const alerta=$(`<div class="alert alert-danger" role="alert" id="datos-incorrectos">
+    const alerta = $(`<div class="alert alert-danger" role="alert" id="datos-incorrectos">
         Los datos ingresados son incorrectos. Intentelo nuevamente </div>`)
 
-        $("form").append(alerta)
-         
-    }
-        
-        
+    $('form').append(alerta)
+  }
 })
 
-function añadirBases(respuestaJSON){
-    const lista = $("#listado-bases")
+function añadirBases (respuestaJSON) {
+  const lista = $('#listado-bases')
 
-    function agregarBase(base){
-        const optionMoneda = $(`<option>${base}</option>`)
+  function agregarBaseDefault (base) {
+    const optionMoneda = $(`<option>${base}</option>`)
 
-        lista.append(optionMoneda)
-    }
+    lista.append(optionMoneda)
+  }
 
-    agregarBase(respuestaJSON.base)
+  agregarBaseDefault(respuestaJSON.base)
 
-    Object.keys(respuestaJSON.rates).forEach(e => {
-        const optionMoneda = $(`<option>${e}</option>`)
+  Object.keys(respuestaJSON.rates).forEach(e => {
+    const optionMoneda = $(`<option>${e}</option>`)
 
-        lista.append(optionMoneda)
-    })
+    lista.append(optionMoneda)
+  })
 }
-
-function validarInputs(fecha, moneda){
-    if(/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/.test(fecha) && moneda.length===3){
-        return true
-    }
-}
-//Cortar equivalencias a 3 decimales
-//Crear tipo cartas para la presentacion de las equivalencias
